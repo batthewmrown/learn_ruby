c908edd467acd47f7d5ff8c5d7ddcf2c403dd8fb
batthewmrown@gmail.com <batthewmrown@gmail.com>
2017-01-28 18:20:24 -0700

    Calculator solution & working tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+  def add(num1, num2)
+    num1 + num2
+  end
+
+  def subtract(num1, num2)
+    num1 - num2
+  end
+
+  def sum(numbers)
+    numbers.inject(0) {|total, number| total + number}
+  end
+
+  def multiply(*num1)
+    num1.inject() {|num1, multiplier| num1 * multiplier}
+  end
+
+  def power(num1, num2)
+    num1 ** num2
+  end
+
+  def factorial(num)
+    if num == 0
+      1
+    else
+      num * factorial(num-1)
+    end
+  end
